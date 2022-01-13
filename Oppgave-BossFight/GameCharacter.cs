0b71914ab40cafdf303339a14f755348fa635484
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-13 13:50:51 +0100

    Add project files.

---
+++ Oppgave-BossFight/GameCharacter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Oppgave_BossFight
+{
+    public class GameCharacter
+    {
+        public int _health;
+        public int _strength;
+        public int _stamina;
+        public bool _alive;
+        public int _maxStamina;
+        public string _name;
+
+        public GameCharacter(int health, int strength, int stamina, string name)
+        {
+            _health = health;
+            _strength = strength;   
+            _stamina = stamina;
+            _alive = true;
+            _maxStamina = stamina;
+            _name = name;
+        }
+
+        public void Fight(GameCharacter enemy)
+        {
+            if (_stamina <= 0)
+            {
+                Recharge();
+            }
+            else
+            {
+                enemy._health -= _strength;
+                _stamina -= 10;
+                Console.WriteLine(enemy._health);
+                if (enemy._health <= 0)
+                {
+                    enemy._alive = false;
+                }
+            }
+        }
+
+        public void Recharge()
+        {
+            _stamina = _maxStamina;
+
+            Console.WriteLine(_name + " Recharging....");
+        }
+    }
+}
---
+++ Oppgave-BossFight/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Oppgave_BossFight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var player1 = new GameCharacter(100, 20, 40,"Moron");
+            var boss = new GameCharacter(400,30, 10,"Boss");
+            var playerTurn = true;
+
+            while (player1._alive && boss._alive)
+            {
+                if (playerTurn)
+                {
+                    player1.Fight(boss);
+                    playerTurn = false;
+                }
+                else
+                {
+                    boss.Fight(player1);
+                    playerTurn = true;
+                } 
+            }
+        }
+    }
+}
